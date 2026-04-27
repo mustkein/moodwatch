a5107b98a7cf663dd1d5c156d42e2aee4f9f6590
keremvarlii <keremvarli1@gmail.com>
2026-04-27 23:20:44 +0300

    feat: javafx desktop iskeleti eklendi

---
+++ desktop/moodwatch-desktop/src/main/java/com/moodwatch/desktop/MoodWatchApp.java
@@ -0,0 +1,23 @@
+package com.moodwatch.desktop;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MoodWatchApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(loader.load(), 1000, 700);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("MoodWatch");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
